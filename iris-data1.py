7912ef28e416ca4719bdc3edd1813a5e4ace8d2b
Babu Baskaran <babubaskaran@gmail.com>
2019-04-25 02:31:11 +0100

    all flowers added

--- iris-data1.py
+++ iris-data1.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 N=50
-x = []
-y = []
 a = []
 b = []
 c = []
 d = []
+e = []
+f = []
 #colors = np.random.rand(N)
 #colors=[np.red.rand, np.yellow.rand]
 
@@ -20,33 +20,33 @@ with open('iris_data.csv','r') as csvfile:
     for row in plots:
        
         if row[4] == "Iris-setosa":
-            x.append(float(row[0]))
-            y.append(float(row[1]))
-#        if row[4] == "Iris-versicolor":
-#            a.append(float(row[0]))
-#            b.append(float(row[1]))
-#        if row[4] == "Iris-virginica":
-#            c.append(float(row[0]))
-#            d.append(float(row[1]))
+            a.append(float(row[0]))
+            b.append(float(row[1]))
+        if row[4] == "Iris-versicolor":
+            c.append(float(row[0]))
+            d.append(float(row[1]))
+        if row[4] == "Iris-virginica":
+            e.append(float(row[0]))
+            f.append(float(row[1]))
     
 
-plt.scatter(x,y, marker="*",label = 'Iris-setosa',color='red')
+plt.scatter(a,b, marker="*",label = 'Iris-setosa',color='red')
 plt.xlabel("Sepal Length")
 plt.ylabel("Sepal Width")
 plt.title('Fishers Iris Data')
 plt.legend()
 
-#plt.scatter(a,b, marker="<", label='Iris-versicolor', color=['blue','orange'])
-#plt.xlabel('Sepal Length')
-#plt.ylabel('Sepal Width')
-# plt.title('Fishers Iris versicolor')
-#plt.legend()
+plt.scatter(c,d, marker=">",label = 'Iris-versicolor',color='blue')
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.title('Fishers Iris versicolor')
+plt.legend()
 
-#plt.scatter(c,d, marker="s", label='Iris-virginica', color=['green','brown'])
-#plt.xlabel('Sepal Length')
-#plt.ylabel('Sepal Width')
-# plt.title('Fishers Iris-virginica')
-#plt.legend()
+plt.scatter(e,f, marker="s",label = 'Iris-virginica',color='green')
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.title('Fishers Iris-virginica')
+plt.legend()
 
 
 plt.show()
