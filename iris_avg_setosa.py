ccf65f3cbba8adf1aef8b2e8e676f53b4abe2744
Babu Baskaran <babubaskaran@gmail.com>
2019-04-26 21:51:27 +0100

    average added correctly

--- iris_avg_setosa.py
+++ iris_avg_setosa.py
@@ -13,6 +13,12 @@ count = 0
 
 # slen = Seapal Length, swid = Seapal Width, plen = Petal Length, pwid = Petal Width
 # slenav = Seapal Length Average, swidav = Seapal Width Average, plenav = Petal Length Average, pwidav = Petal Width Average
+
+a = []
+b = []
+c = []
+d = []
+count = 0
 slen = 0
 swid = 0
 plen = 0
@@ -22,7 +28,7 @@ swidav = 0
 plenav = 0
 pwidav = 0
 
-
+# function for calculating the average of the length & width
 def avg(X,Y):
     avg = 0
     avg = X/Y
@@ -36,15 +42,15 @@ with open('irisdata.csv','r') as csvfile:
         if row[4] == "Iris-setosa":
             a.append(float(row[0]))
             b.append(float(row[1]))
-            c.append(float(row[0]))
-            d.append(float(row[1]))
+            c.append(float(row[2]))
+            d.append(float(row[3]))
 
             count += 1
             
             slen += float(row[0])
-            swid += float(row[0])
-            plen += float(row[0])
-            pwid += float(row[0])
+            swid += float(row[1])
+            plen += float(row[2])
+            pwid += float(row[3])
 
 slenav = avg(slen,count)
 swidav = avg(swid,count)
@@ -52,12 +58,12 @@ plenav = avg(plen,count)
 pwidav = avg(pwid,count)
 
 
-plt.scatter(slenav,swidav, marker="^", label='Iris-setosa', color=['red'])
-plt.scatter(plenav,pwidav, marker="<", label='Iris-setosa', color=['green'])
+plt.scatter(slenav,swidav, marker="^", label='Iris-setosa-Sepal', color=['red'])
+plt.scatter(plenav,pwidav, marker="<", label='Iris-setosa-Petal', color=['green'])
 
 
-plt.xlabel("Average Length")
-plt.ylabel("Average Width")
+plt.ylabel("Average Length")
+plt.xlabel("Average Width")
 plt.title('Fishers Iris Setosa')
 plt.legend()
 plt.show()
