22b0b71e3d042d0c0ce34fd16a76ae3202e3d052
Babu Baskaran <babubaskaran@gmail.com>
2019-04-27 20:05:11 +0100

    iris data analysis continue

--- iris_data_analysis.py
+++ iris_data_analysis.py
@@ -6,13 +6,40 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
+from pandas.plotting import scatter_matrix
+
 dafile = "irisdata.csv"
 
 # defining headers to the file
-columns = ['Sepal-Length','Sepal-Width','Petal-Length','Petal-Width','Species']
+colms = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Species']
 
-datafile = pd.read_csv(dafile,names=columns)
+datafile = pd.read_csv(dafile,names=colms)
 
 datafile.info()
 
 print(datafile.groupby('Species').size())
+
+print(datafile.describe())
+
+datafile.plot(kind='box', subplots = True, layout = (1,4), sharex = False, sharey = False, title='Sum Box Plot')
+
+fig, ax = plt.subplots()
+
+datafile.boxplot(column= ['Sepal_Length'], ax=ax)
+
+#sns.boxplot(  x= "c",y="a",data=df , orient = "h")
+
+fig, ax = plt.subplots()
+datafile.boxplot(column=['Petal_Length'], by='Species', ax=ax)
+
+fig, ax = plt.subplots()
+datafile.boxplot(column=['Sepal_Width'], by='Species', ax=ax)
+
+fig, ax = plt.subplots()
+datafile.boxplot(column=['Petal_Width'], by='Species', ax=ax)
+
+datafile.hist(bins=10)
+
+scatter_matrix(datafile)
+
+plt.show()
